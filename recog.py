b0ed2e7874fb15a081c7088f9aeb4e2413742761
lpkpaco <50955568+lpkpaco@users.noreply.github.com>
2024-01-27 17:14:32 +0800

    Add files via upload

--- recog.py
+++ recog.py
@@ -1,44 +1,71 @@
-from ultralytics import YOLO
+import torch
+import numpy as np
 import cv2
-import cvzone
-import math
+import pafy
 import time
- 
-cap = cv2.VideoCapture(1)
-cap.set(3, 1280)
-cap.set(4, 720)
- 
- 
-model = YOLO("best.pt")
- 
-classNames = ["redfish", "blackhead", "eaststar", "horsefriend", "redfish"]
- 
-prev_frame_time = 0
-new_frame_time = 0
- 
-while True:
-    new_frame_time = time.time()
-    success, img = cap.read()
-    results = model(img, stream=True)
-    for r in results:
-        boxes = r.boxes
-        for box in boxes:
-            # Bounding Box
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
-            w, h = x2 - x1, y2 - y1
-            cvzone.cornerRect(img, (x1, y1, w, h))
-            # Confidence
-            conf = math.ceil((box.conf[0] * 100)) / 100
-            # Class Name
-            cls = int(box.cls[0])
 
-            cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=1, thickness=1)
- 
-    fps = 1 / (new_frame_time - prev_frame_time)
-    prev_frame_time = new_frame_time
-    print(fps)
- 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+
+class ObjectDetection:
+    
+    def __init__(self):
+        self.model = self.load_model()
+        self.classes = self.model.names
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print("\n\nDevice Used:",self.device)
+    def load_model(self):
+
+        model = torch.hub.load('best.pt', 'yolov5s', pretrained=True)
+        return model
+
+
+    def score_frame(self, frame):
+        self.model.to(self.device)
+        frame = [frame]
+        results = self.model(frame)
+     
+        labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
+        return labels, cord
+
+
+    def class_to_label(self, x):
+        return self.classes[int(x)]
+
+
+    def plot_boxes(self, results, frame):
+        labels, cord = results
+        n = len(labels)
+        x_shape, y_shape = frame.shape[1], frame.shape[0]
+        for i in range(n):
+            row = cord[i]
+            if row[4] >= 0.2:
+                x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
+                bgr = (0, 255, 0)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
+                cv2.putText(frame, self.class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
+
+        return frame
+
+
+    def __call__(self):
+        cap = cv2.VideoCapture(0)
+
+        while cap.isOpened():
+            
+            start_time = time.perf_counter()
+            ret, frame = cap.read()
+            if not ret:
+                break
+            results = self.score_frame(frame)
+            frame = self.plot_boxes(results, frame)
+            end_time = time.perf_counter()
+            fps = 1 / np.round(end_time - start_time, 3)
+            cv2.putText(frame, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+            cv2.imshow("img", frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+
+# Create a new object and execute.
+detection = ObjectDetection()
+detection()
